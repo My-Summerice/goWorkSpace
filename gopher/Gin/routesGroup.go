cbcfea0f9896545bf055837c702e91c0908d33b8
summerice <1146494446@qq.com>
2022-06-01 22:32:46 +0800

    update

--- gopher/Gin/routesGroup.go
+++ gopher/Gin/routesGroup.go
@@ -13,13 +13,13 @@ func main() {
 	v1 := r.Group("/v1")
 	{
 		v1.GET("/login", login)
-		v1.GET("submit", submit)
+		v1.GET("/submit", submit)
 	}
 	// 路由组2，处理POST请求
 	v2 := r.Group("/v2")
 	{
 		v2.POST("/login", login)
-		v2.POST("submit", submit)
+		v2.POST("/submit", submit)
 	}
 	r.Run(":12345")
 }
