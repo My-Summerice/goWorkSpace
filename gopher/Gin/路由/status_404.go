eb26211e82b2857cd1f4a562288b8f0fabe124f3
summerice <1146494446@qq.com>
2022-06-02 13:50:26 +0800

    update

--- gopher/Gin/路由/status_404.go
+++ gopher/Gin/路由/status_404.go
@@ -9,16 +9,16 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/user", notFound)
-	r.NoRoute(printMessage) 
+	r.GET("/user", printMessage)
+	r.NoRoute(printNotFound) 
 	r.Run()
 }
 
-func notFound(c *gin.Context) {
+func printMessage(c *gin.Context) {
 	message := c.DefaultQuery("message", "是的，你成功了，这里没有404！")
 	c.String(http.StatusOK, fmt.Sprintf("运行结果怎么样呢？\n%s", message))
 }
 
-func printMessage(c *gin.Context) {
+func printNotFound(c *gin.Context) {
 	c.String(http.StatusNotFound, "404 NOT FOUND by summerice")
 }
