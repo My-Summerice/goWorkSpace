a3bebcc77815e5fb656eb27a78435af1471683da
summerice <1146494446@qq.com>
2022-06-17 23:09:19 +0800

    update

--- gopher/Gin/parameterValidation/validatorV10Test.go
+++ gopher/Gin/parameterValidation/validatorV10Test.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
     "net/http"
-    "time"
+    _"time"
 
     "github.com/gin-gonic/gin"
-    "github.com/gin-gonic/gin/binding"
+    _"github.com/gin-gonic/gin/binding"
     "github.com/go-playground/validator/v10"
 )
 
@@ -26,12 +26,47 @@ Validator 是基于 tag（标记）实现结构体和单个字段的值验证库
     Web 框架 gin 的默认验证器。
 */
 
+// 定义一个添加用户参数结构体
+type AddUser struct {
+    Name    string  `form:"name" validate:"required"`
+    Email   string  `form:"email" validate:"required"`
+    Age     uint8   `form:"age" validate:"required,gte=15,lte=30"`
+}
+
+func main() {
+    r := gin.Default()
+    r.GET("/addUser", func(c *gin.Context) {
+        var addUser AddUser
+        // 解析并绑定数据
+        err := c.ShouldBind(&addUser)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+            return
+        }
+        /*  v     值的默认格式
+            %+v   添加字段名(如结构体就先输出结构体的字段类型，再输出该字段的值)
+            %#v　 相应值的Go语法表示   */
+        fmt.Printf("addUser: %#v\n", addUser)
+        // 使用Validator验证
+        validate := validator.New()
+        err = validate.Struct(addUser)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+            return
+        }
+        c.JSON(http.StatusOK, gin.H{"msg": "add success!"})
+    })
+
+    r.Run(":8888")
+}
+
 /*
 当涉及到一些复杂的校验规则，这些已有的校验规则就不能满足我们的需求了。
 例如现在有一个需求，存在db的用户信息中创建时间与更新时间都要大于某一时间，假设是从前端传来的（当然不可能，哈哈）。
 现在我们来写一个简单示例，学习一下怎么对这个参数进行校验。
 */
 
+/* 有bug
 type Info struct {
     createTime  time.Time   `form:"createTime" binding:"required,timing" time_format:"2006-01-02"`
     updateTime  time.Time   `form:"updateTime" binding:"required,timing" time_format:"2006-01-02"`
@@ -53,6 +88,7 @@ func main() {
     r := gin.Default()
     // 注册验证
     if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+        // 注册自定义标签
         err := v.RegisterValidation("timing", timing)
         if err == nil {
             fmt.Println("success")
@@ -72,3 +108,4 @@ func getTime(c *gin.Context) {
     }
     c.JSON(http.StatusOK, "数据正确")
 }
+*/
