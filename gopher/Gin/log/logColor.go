b5f11243117f4866b314db8ceadc4ea3efdb1bbf
summerice <1146494446@qq.com>
2022-06-20 23:15:52 +0800

    update

--- gopher/Gin/log/logColor.go
+++ gopher/Gin/log/logColor.go
@@ -10,12 +10,14 @@ import (
 func main() {
 	// 禁止日志颜色
 	gin.DisableConsoleColor()
+	// 强制日志着色
+	//gin.ForceConsoleColor()
 
 	// logging to a file.
 	f, _ := os.Create("gin.log")
 	//gin.DefaultWriter = io.MultiWriter(f)
 	// 如果需要同时将日至写入文件和控制台，使用以下代码
-	 gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 
 	r := gin.Default()
 	r.GET("/logging", func(c *gin.Context) {
